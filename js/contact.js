a85eb2e47a96d5fc299c8ad70c41793371da6521
DrannikovVladimir <30643599+DrannikovVladimir@users.noreply.github.com>
2018-07-04 21:21:26 +0600

    JavaScript
    Проблема с отображением интерактивной карты

--- js/contact.js
+++ js/contact.js
@@ -1,15 +1,35 @@
 var writeUs = document.querySelector(".button-contacts");
 var popupContact = document.querySelector(".modal-contact");
 var closeContact = document.querySelector(".button-close-contact");
-var form = popupContact.querySelector(".contact-form");
+
+var userForm = popupContact.querySelector(".contact-form");
 var userName = popupContact.querySelector("[name=username]");
-var email = popupContact.querySelector("[name=email]");
-var message = popupContact.querySelector("[name=message]");
+var userEmail = popupContact.querySelector("[name=email]");
+var userMessage = popupContact.querySelector("[name=message]");
+
+var storageName = "";
+var storageEmail = "";
+var isStorageSupport = true;
+
+try {
+  storageName = localStorage.getItem("userName");
+  storageEmail = localStorage.getItem("userEmail");
+} catch (err) {
+  isStorageSupport = false;
+}
 
 writeUs.addEventListener("click", function(evt) {
   evt.preventDefault();
   popupContact.classList.add("modal-show");
-  userName.focus();
+  if (storageName) {
+    userName.value = storageName;
+    if (storageEmail) {
+      userEmail.value = storageEmail;
+      userMessage.focus();
+    }
+  } else {
+    userName.focus();
+  }
 });
 
 closeContact.addEventListener("click", function (evt) {
@@ -17,9 +37,22 @@ closeContact.addEventListener("click", function (evt) {
   popupContact.classList.remove("modal-show");
 });
 
-form.addEventListener("submit", function(evt) {
-  evt.preventDefault();
-  if (!userName || !email || !message) {
-    console.log("Заполните данные формы")
+userForm.addEventListener("submit", function(evt) {
+  if (!userName.value || !userEmail.value) {
+    evt.preventDefault();
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("userName", userName.value);
+      localStorage.setItem("userEmail", userEmail.value);
+    }
   }
-})
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popupContact.classList.contains("modal-show")) {
+      popupContact.classList.remove("modal-show");
+    }
+  }
+});
--- js/map.js
+++ js/map.js
@@ -1,8 +1,8 @@
-var mapLink = document.querySelector(".map-link");
+var linkMap = document.querySelector(".map-link");
 var popupMap = document.querySelector(".modal-map");
 var closeMap = document.querySelector(".button-close-map");
 
-mapLink.addEventListener("click", function(evt) {
+linkMap.addEventListener("click", function(evt) {
   evt.preventDefault();
   popupMap.classList.add("modal-show");
 });
