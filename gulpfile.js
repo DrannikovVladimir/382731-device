2af22c3bc2c9774c6956c7e8614723934756c7d7
Vladimir Drannikov <krowen@inbox.ru>
2019-08-22 16:51:02 +0600

    Оживил табы в секции сервисы

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+
+gulp.task('server', function() {
+  browserSync.init({
+    server: "./"
+  });
+  gulp.watch('./css/**/*.css').on('change', browserSync.reload);
+  gulp.watch('./**/*.html').on('change', browserSync.reload);
+});
+
+gulp.task('start', gulp.series('server'));
